1b3854de10aefd61569850faada5a971765541fb
wilfriedaugeard <wilfried.augeard@etu.u-bordeaux.fr>
2020-11-12 20:02:19 +0100

    Mise en place des tasks

--- src/server.js
+++ src/server.js
@@ -251,6 +251,21 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
             
         })
 
+        /*************************************/
+        /*                                   */
+        /*               TASKS               */ 
+        /*                                   */ 
+        /*************************************/
+
+        app.get('/projectView/:projectId/tasks', (req, res) => {
+            const cursor =  db.collection('projects').findOne({"_id":ObjectId(req.params.projectId)})
+            .then(results => {
+                res.render('pages/task.ejs', {project: results})
+            })
+            .catch(error => console.error(error))
+            
+        })
+
 
 
         app.listen(3000, function () {
