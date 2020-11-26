c8af99eb472b1194727a45e1c0fb90d80aee81b2
Thimotée Brocard <brocard.thimotee@gmail.com>
2020-11-26 14:35:17 +0100

    creation US test

--- src/cypress/integration/inte/US_test.js
+++ src/cypress/integration/inte/US_test.js
@@ -1,6 +1,19 @@
-const jeSouhaiteCreated='faire quelque chose';
-const afinDeCreated='pouvoir faire des trucs';
-const jeSouhaiteModified='faire autre chose'
+const enTantQue = 'En tant que ';
+const jeSouhaite = ', je souhaite ';
+const afinDe = ' afin de ';
+
+const roleCreation = 'Développeur';
+const jeSouhaiteCreated = 'faire quelque chose';
+const afinDeCreated = 'pouvoir faire des trucs';
+const importanceCreation = 'Moyenne-Haute';
+const difficultCreation = '5';
+const planificationCreation = '-';
+
+const jeSouhaiteAnnul = 'ne pas faire quelque chose';
+const afinDeAnnul = 'ne pas pouvoir faire des trucs';
+
+
+const jeSouhaiteModified = 'faire autre chose';
 
 describe('Gestion backlog', () => {
     it('Charge la page d\'acceuil', () => {
@@ -15,15 +28,41 @@ describe('Gestion backlog', () => {
         cy.url().should('include', '/projectView/');
         cy.url().should('include', '/backlog');
     })
-    it('Charge la page de backlog', () => {
+    it('Création d\'une US', () => {
         cy.get('.container-fluid')
-        .get('.spacex1')
-        .get('.table')
-        .get('.table-hover')
-        .get('.table-sm')
-        .get('.table-striped')
-        .get('.ui-sortable').children().then(($childrenBefore) => {
-            
-        })
+            .get('.spacex1')
+            .get('.table')
+            .get('.table-hover')
+            .get('.table-sm')
+            .get('.table-striped')
+            .get('.ui-sortable').children().then(($childrenBefore) => {
+                cy.get('.btn-sm').click();
+                cy.get('.modal-body:nth-child(1) #entantque').select(roleCreation);
+                cy.get('.modal-body:nth-child(1) #jesouhaite').click();
+                cy.get('.modal-body:nth-child(1) #jesouhaite').type(jeSouhaiteCreated);
+                cy.get('.modal-body:nth-child(1) #afinde').click();
+                cy.get('.modal-body:nth-child(1) #afinde').type(afinDeCreated);
+                cy.get('.modal-body:nth-child(1) #importance').select(importanceCreation);
+                cy.get('.modal-body:nth-child(1) #difficulte').select(difficultCreation);
+                cy.get('.form-group:nth-child(4) > #plannification').select(planificationCreation);
+                cy.get('.modal-footer:nth-child(2) > .btn-success').click();
+                cy.get('.container-fluid')
+                    .get('.spacex1')
+                    .get('.table')
+                    .get('.table-hover')
+                    .get('.table-sm')
+                    .get('.table-striped')
+                    .get('.ui-sortable').get('.ui-sortable-handle')
+                    .should('contain', enTantQue + roleCreation + jeSouhaite + jeSouhaiteCreated + afinDe + afinDeCreated);
+                cy.get('.container-fluid')
+                    .get('.spacex1')
+                    .get('.table')
+                    .get('.table-hover')
+                    .get('.table-sm')
+                    .get('.table-striped')
+                    .get('.ui-sortable').children().then(($childrenAfter) => {
+                        expect($childrenBefore.length + 1).to.equal($childrenAfter.length);
+                    })
+            })
     })
 })
