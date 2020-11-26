646e68bb3862cdb4bcc6799a5a399c3dc3eba3f0
Thimotée Brocard <brocard.thimotee@gmail.com>
2020-11-26 13:30:00 +0100

    debut tests US/backlog

--- src/cypress/integration/inte/US_test.js
+++ src/cypress/integration/inte/US_test.js
@@ -1,17 +1,15 @@
-describe('Page d\'acceuil', () => {
+describe('Gestion sprint', () => {
     it('Charge la page d\'acceuil', () => {
         cy.visit('/')
+        cy.url().should('include', '/projectList')
     })
-    it('Création de projet validée', () => {
-        cy.get('.btn-sm').click();
-        cy.get('#projectName').click();
-        cy.get('#projectName').type('Création de projet avec cypress');
-        cy.get('#projectDesc').click();
-        cy.get('#projectDesc').type('on crée un projet test');
-        cy.get('#sprintDelay').click();
-        cy.get('#sprintDelay').type('{backspace}4');
-        cy.get('#dateEnd').click();
-        cy.get('#dateEnd').type('2020-11-29');
-        cy.get('.btn-success').click();
-        cy.url().should('contains', 'http://localhost:3000/projectList');
+    it('Charge la page de backlog', () => {
+        cy.get('.clickable-row').contains('Projet Test').click();
+        cy.url().should('include', '/projectView/');
+        cy.get('.nav-item').should('contain', 'Backlog');
+        cy.get('.nav-item').contains('Backlog').click();
+        cy.url().should('include', '/projectView/');
+        cy.url().should('include', '/backlog');
     })
+    
+})
