28101bccedbf7328022b55ccc6e910e40609505f
Thimotée Brocard <brocard.thimotee@gmail.com>
2020-11-26 11:44:29 +0100

    Sprint test propre

--- src/cypress/integration/inte/sprint_test.js
+++ src/cypress/integration/inte/sprint_test.js
@@ -7,30 +7,38 @@ describe('Gestion sprint', () => {
         cy.url().should('include', '/projectView/');
         cy.get('.nav-item').should('contain', 'Sprints');
         cy.get('.nav-item').contains('Sprints').click();
-        cy.url().should('include', '/projectView/'); 
-        cy.url().should('include', '/sprint'); 
+        cy.url().should('include', '/projectView/');
+        cy.url().should('include', '/sprint');
     })
     it('Création de sprint', () => {
-        cy.get('.btn-sm').click();
-        cy.get('#beginDate').click();
-        cy.get('#beginDate').type('2020-11-29');
-        cy.get('#description').click();
-        cy.get('#description').type('Sprint 1 pour tester');
-        cy.get('.btn-success').click();
-        cy.url().should('include', '/projectView/'); 
-        cy.url().should('include', '/sprint');
-        cy.get('.container-fluid').get('.spacex1').should('contain', 'Sprint');
-        //On devrait regarder s'il y a un sprint en plus automatiquement?        
+        cy.get('.container-fluid').get('.spacex1').children().then(($childrenBefore) => {
+            cy.get('.btn-sm').click();
+            cy.get('#beginDate').click();
+            cy.get('#beginDate').type('2020-11-29');
+            cy.get('#description').click();
+            cy.get('#description').type('Sprint 1 pour tester');
+            cy.get('.btn-success').click();
+            cy.url().should('include', '/projectView/');
+            cy.url().should('include', '/sprint');
+            cy.get('.container-fluid').get('.spacex1').should('contain', 'Sprint');
+            cy.get('.container-fluid').get('.spacex1').children().then(($childrenAfter) => {
+                expect($childrenBefore.length + 1).to.equal($childrenAfter.length);
+            })
+        })
     })
     it('Création de sprint annulé', () => {
-        cy.get('.btn-sm').click();
-        cy.get('#beginDate').click();
-        cy.get('#beginDate').type('2020-11-29');
-        cy.get('#description').click();
-        cy.get('#description').type('Sprint 2 pour tester');
-        cy.get('.btn-danger').click();
-        cy.url().should('include', '/projectView/'); 
-        cy.url().should('include', '/sprint');
-        //On devrait regarder s'il y a pas de sprint en plus automatiquement?        
+        cy.get('.container-fluid').get('.spacex1').children().then(($childrenBefore) => {
+            cy.get('.btn-sm').click();
+            cy.get('#beginDate').click();
+            cy.get('#beginDate').type('2020-11-29');
+            cy.get('#description').click();
+            cy.get('#description').type('Sprint 2 pour tester');
+            cy.get('.btn-danger').click();
+            cy.url().should('include', '/projectView/');
+            cy.url().should('include', '/sprint');
+            cy.get('.container-fluid').get('.spacex1').children().then(($childrenAfter) => {
+                expect($childrenBefore.length).to.equal($childrenAfter.length);
+            })
+        })
     })
 })
