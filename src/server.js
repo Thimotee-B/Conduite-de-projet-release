ed36679c031f96e40cd0e38c5a568f8aa174ee85
Thimotée Brocard <brocard.thimotee@gmail.com>
2020-11-26 14:39:52 +0100

    ajout server local (a enlever) pour mes tests

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser")
 const MongoClient = require("mongodb").MongoClient
 const ObjectId = require("mongodb").ObjectID
 
-const connectionString = "mongodb+srv://cdp2020:cdp2020@clustercdp.8wan9.mongodb.net/cdp2020?retryWrites=true&w=majority"
+const connectionString ="mongodb://127.0.0.1:27017/" //"mongodb+srv://cdp2020:cdp2020@clustercdp.8wan9.mongodb.net/cdp2020?retryWrites=true&w=majority" 
 
 app.use(express.static(path.join(__dirname, "/public")))
 app.use(fileUpload())
