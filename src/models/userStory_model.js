bf21f8857f9e289dfebd0be0d281c231bd3318c0
Dysta <xav4098@gmail.com>
2020-12-06 11:35:52 +0100

    patch bug gestion US suppression

--- src/models/userStory_model.js
+++ src/models/userStory_model.js
@@ -1,3 +1,5 @@
+const taskModel = require("./task_model")
+
 function updateUserStoryNumber(db, projectId, userStoryNumber) {
     return db.collection("projects").updateOne(
         { _id: projectId },
@@ -87,12 +89,52 @@ function deleteUserStoryAtPos(db, project, pos) {
         .catch(error => console.error(error))
 }
 
-
+function removeUsRefFromTask(db, project, objectId, pos){
+    for(let i = 0; i<project.task.length; i++){
+        for(let j=0; j<project.task[i].usRef.length; j++){
+            if(project.task[i].usRef[j] == project.us[pos].id){
+                const taskPos       = i
+                const taskId        = project.task[taskPos].id
+                const dep           = project.task[taskPos].dep
+                const description   = project.task[taskPos].description
+                const dod           = project.task[taskPos].dod
+                let usRef           = project.task[taskPos].usRef
+                console.log(usRef)
+                usRef.splice(j,1)
+                console.log(usRef)
+                const duree         = project.task[taskPos].duree
+                const etat          = project.task[taskPos].etat
+                const taskTotal     = project.task[taskPos].taskTotal
+                const taskDone      = project.task[taskPos].taskDone
+                
+                taskModel.updateTaskByPos(
+                    db,
+                    objectId,
+                    taskPos,
+                    taskId,
+                    dep,
+                    description,
+                    dod,
+                    usRef,
+                    duree,
+                    etat,
+                    taskTotal,
+                    taskDone
+                )
+                .then((val) => {
+                    console.log(val)
+                })
+                break;
+            }
+        }
+    }
+}
 
 
 module.exports = {
     updateUserStoryNumber,
     insertUserStory,
     insertUserStoryAtPos,
-    deleteUserStoryAtPos
+    deleteUserStoryAtPos,
+    removeUsRefFromTask
 }
