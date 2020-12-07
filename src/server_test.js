a72df8d569ccfd1829bd4b9780f943b6ae12373f
Thimotée Brocard <brocard.thimotee@gmail.com>
2020-12-07 08:57:52 +0100

    edit : ajout controller - AUCUNE US

--- src/server_test.js
+++ src/server_test.js
@@ -25,6 +25,7 @@ const sprintController      = require("./controllers/sprint_controller.js")
 const releaseController     = require("./controllers/release_controller.js")
 const taskController        = require("./controllers/task_controller.js")
 const roleController        = require("./controllers/role_controller.js")
+const docController         = require("./controllers/doc_controller.js")
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
@@ -50,6 +51,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
         releaseController.init(app, db, ObjectId)
 
         roleController.init(app, db, ObjectId)
+
+        docController.init(app, db, ObjectId)
 
         app.listen(port, function () {
             console.log("listening on "+port)
