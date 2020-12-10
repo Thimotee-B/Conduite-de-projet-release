3b1e22e58c2456a3a7660cdfa95cf5ce028aff39
wilfriedaugeard <wilfried.augeard@etu.u-bordeaux.fr>
2020-12-10 18:13:44 +0100

    Documentation pour les controllers - AUCUNE US

--- src/controllers/doc_controller.js
+++ src/controllers/doc_controller.js
@@ -1,6 +1,16 @@
 const projectModel = require("../models/project_model")
 const docModel = require("../models/doc_model")
+/**
+ * @namespace Controller_Documentation
+ */
 
+/**
+ * Manage root on documentation page.
+ * @memberof Controller_Documentation
+ * @param {function} app - Express application.
+ * @param {object} db - Database object.
+ * @param {function} ObjectId - Function from mongoDB.
+*/
 function init(app, db, ObjectId) {
     app.get("/projectView/:projectId/doc", async (req, res) => {
         const project = await projectModel.getProjectId(db, ObjectId(req.params.projectId))
--- src/controllers/view_controller.js
+++ src/controllers/view_controller.js
@@ -1,5 +1,15 @@
 const projectModel = require("../models/project_model")
+/**
+ * @namespace Controller_view
+ */
 
+/**
+ * Manage project and redirect on dashboad page.
+ * @memberof Controller_view
+ * @param {function} app - Express application.
+ * @param {object} db - Database object.
+ * @param {function} ObjectId - Function from mongoDB.
+ */
 function init(app, db, ObjectId) {
     app.get("/projectView/:projectId/accueil", async (req, res) => {
         const project = await projectModel.getProjectId(db, ObjectId(req.params.projectId))
